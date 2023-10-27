4daa6024dc19ce28a31cdbadd85f25060506e8e8
msiddu04 <83287310+msiddu04@users.noreply.github.com>
2023-10-27 12:56:38 +0530

    Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -5,13 +5,13 @@ class Factorial {
     static int factorial(int n) 
     { 
         int res = 1, i; 
-        for (i = 2; i <= n; i++) 
+        for (i = 2; i <= n; i++) //  logical code for factorial of number
             res *= i; 
         return res; 
     } 
   
 
-    public static void main(String[] args) 
+    public static void main(String[] args)   // main method for input operation
     {  
 	Scanner scr = new Scanner(System.in);
 	System.out.println("enter factorial number");
