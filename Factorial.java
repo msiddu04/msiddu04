8a0e46ae4a4bc9a68db35a49073d0a13ae778412
msiddu04 <83287310+msiddu04@users.noreply.github.com>
2023-10-27 12:53:10 +0530

    factorial of a number

---
+++ Factorial.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+class Factorial { 			
+
+    static int factorial(int n) 
+    { 
+        int res = 1, i; 
+        for (i = 2; i <= n; i++) 
+            res *= i; 
+        return res; 
+    } 
+  
+
+    public static void main(String[] args) 
+    {  
+	Scanner scr = new Scanner(System.in);
+	System.out.println("enter factorial number");
+	int num = scr.nextInt();
+        System.out.println("Factorial of " + num + " is "
+                           + factorial(num)); 
+    } 
+}
